31f711be42543c4734db557dd7e24492cd80cd65
Neel <neelp0980@gmail.com>
2024-07-17 02:10:55 +0530

    feat(blink): add jupiter dca sdk

---
+++ src/app/api/actions/contant.ts
@@ -0,0 +1,15 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "DmSJm2P2KSRx5MrqU9ukqBGhopNiJRW3VSRV8Bmqagbp"
+);
+
+export const USDC = new PublicKey(
+  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+);
+
+export const BONK = new PublicKey(
+  "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
---
+++ src/app/api/actions/dca/route.ts
@@ -0,0 +1,184 @@
+/**
+ * Solana Actions Example
+ */
+
+import {
+  ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+  createPostResponse,
+  ActionGetResponse,
+  ActionPostRequest,
+} from "@solana/actions";
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+} from "@solana/web3.js";
+import { BONK, DEFAULT_SOL_ADDRESS, DEFAULT_SOL_AMOUNT, USDC } from "../contant";
+import { CreateDCAParamsV2, DCA, Network } from "@jup-ag/dca-sdk";
+
+export const GET = async (req: Request) => {
+  try {
+    const requestUrl = new URL(req.url);
+    const { toPubkey } = validatedQueryParams(requestUrl);
+
+    const baseHref = new URL(
+      `/api/actions/dca?to=${toPubkey.toBase58()}`,
+      requestUrl.origin
+    ).toString();
+
+    const payload: ActionGetResponse = {
+      title: "DCA - SOL",
+      icon: new URL("https://i.postimg.cc/sXw7cxP8/20231124065326-df50bee7-c127-4acb-af58-b8616e21acd0-152.jpg", requestUrl.origin).toString(),
+      description: "DCA SOL at your finger tips",
+      label: "Order",
+      links: {
+        actions: [
+          {
+            label: "DCA 5 USDC",
+            href: `${baseHref}&amount=${"10"}`,
+          },
+          {
+            label: "DCA 15 USDC",
+            href: `${baseHref}&amount=${"25"}`,
+          },
+          {
+            label: "DCA 50 USDC",
+            href: `${baseHref}&amount=${"50"}`,
+          },
+          {
+            label: "DCA USDC",
+            href: `${baseHref}&amount={amount}`,
+            parameters: [
+              {
+                name: "amount",
+                label: "Enter the amount of USDC to DCA SOL",
+                required: true,
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    return Response.json(payload, {
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  } catch (err) {
+    console.log(err);
+    let message = "An unknown error occurred";
+    if (typeof err == "string") message = err;
+    return new Response(message, {
+      status: 400,
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  }
+};
+
+export const OPTIONS = GET;
+
+export const POST = async (req: Request) => {
+  try {
+    const requestUrl = new URL(req.url);
+    const { amount, toPubkey } = validatedQueryParams(requestUrl);
+
+    const body: ActionPostRequest = await req.json();
+
+    // validate the client provided input
+    let account: PublicKey;
+    try {
+      account = new PublicKey(body.account);
+    } catch (err) {
+      return new Response('Invalid "account" provided', {
+        status: 400,
+        headers: ACTIONS_CORS_HEADERS,
+      });
+    }
+
+    const connection = new Connection("https://api.mainnet-beta.solana.com");
+
+    // ensure the receiving account will be rent exempt
+    const minimumBalance = await connection.getMinimumBalanceForRentExemption(
+      0 // note: simple accounts that just store native SOL have `0` bytes of data
+    );
+    if (amount * LAMPORTS_PER_SOL < minimumBalance) {
+      throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
+    }
+
+    const transaction = new Transaction();
+
+    const dca = new DCA(connection, Network.MAINNET);
+
+    const params: CreateDCAParamsV2 = {
+      payer: toPubkey, 
+      user: toPubkey,
+      inAmount: BigInt(amount), 
+      inAmountPerCycle: BigInt(amount),
+      cycleSecondsApart: BigInt(86400), // 1 day between each order -> 60 * 60 * 24
+      inputMint: USDC,
+      outputMint: BONK,
+      minOutAmountPerCycle: null, 
+      maxOutAmountPerCycle: null,
+      startAt: null,
+    };
+
+    const { tx, dcaPubKey } = await dca.createDcaV2(params);
+
+    transaction.add(tx);
+
+    // set the end user as the fee payer
+    transaction.feePayer = account;
+
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        transaction,
+        message: `Send ${amount} SOL to ${toPubkey.toBase58()}`,
+      },
+    });
+
+    return Response.json(payload, {
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  } catch (err) {
+    console.log(err);
+    let message = "An unknown error occurred";
+    if (typeof err == "string") message = err;
+    return new Response(message, {
+      status: 400,
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  }
+};
+
+function validatedQueryParams(requestUrl: URL) {
+  let toPubkey: PublicKey = DEFAULT_SOL_ADDRESS;
+  let amount: number = DEFAULT_SOL_AMOUNT;
+
+  try {
+    if (requestUrl.searchParams.get("to")) {
+      toPubkey = new PublicKey(requestUrl.searchParams.get("to")!);
+    }
+  } catch (err) {
+    throw "Invalid input query parameter: to";
+  }
+
+  try {
+    if (requestUrl.searchParams.get("amount")) {
+      amount = parseFloat(requestUrl.searchParams.get("amount")!);
+    }
+
+    if (amount <= 0) throw "amount is too small";
+  } catch (err) {
+    throw "Invalid input query parameter: amount";
+  }
+
+  return {
+    amount,
+    toPubkey,
+  };
+}
